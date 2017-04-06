86bb2f48ffe90e996e3b2d6083df484df224d021
Kuhn <vso513@784f437f9a8c.cof.ds.capitalone.com>
2017-04-06 09:12:37 -0400

    updating java 2 folder

--- java2/src/main/java/com/example/Lab.java
+++ java2/src/main/java/com/example/Lab.java
@@ -11,9 +11,22 @@ public class Lab {
 	  f(1) = 1
 	  f(10) = 55
 	*/
-	public static int fibonacci(int n) {
-		return 0;
-	}
+	public static int fibonacci(int n) { //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+		int x = 0;
+		int y = 1;
+
+		if (n==0) {
+			y = 0;
+		}
+		for (int i = 1; i <= n; i++) {
+
+			if (i % 2 == 0) {
+				x = x + y;
+			}
+			else y = x + y;
+		}
+		return(Math.max(x, y));}
 
 	/*
 	  2. Sort array of integers
@@ -21,8 +34,35 @@ public class Lab {
 	  Don't use built-in sort() method... that would be lame.
 	*/
 	public static int[] sort(int[] array) {
-		return null;
+
+		 if (array.length < 2) {
+		 return array;
+		   }
+
+		int cnt = 0;
+		int p0[] = Arrays.copyOf(array, array.length);
+
+		if (array.length < 3) {
+			if(p0[cnt] > array[array.length-1]) {
+				int temp = p0[cnt];
+				p0[cnt] = array[array.length-1];
+				p0[cnt+1] = temp;}
+
+		}
+		else while (cnt < array.length-1) {
+
+			if(p0[cnt] > array[array.length-1]) {
+
+				int temp = p0[cnt];
+				p0[cnt] = array[array.length-1];
+				p0[cnt+1] = temp;
+			}
+			cnt = cnt + 1;
+
+		}
+		return p0;
 	}
+
 
 	/*
 	  3. Return the factorial of n
@@ -31,7 +71,19 @@ public class Lab {
 	  f(3) = 6
 	*/
 	public static int factorial(int n) {
-		return 0;
+
+		int fSum = 1;
+
+		if (n < 2) {
+
+			fSum = 1;
+		}
+		while (n > 1){
+			fSum = fSum * n;
+			n = n-1;
+
+		}
+		return fSum;
 	}
 
 	/*
@@ -42,8 +94,25 @@ public class Lab {
 	  f([1,2,3,4,5], 3) = [4,5,1,2,3]
 	*/
 	public static int[] rotateLeft(int[] array, int n) {
-		return null;
+
+		int newOne[] = Arrays.copyOf(array, array.length);
+		int len = array.length;
+		int c = 0;
+		if (n>len) {
+			n= n-len;
+		}
+		for (int i = 0; i < len; i++) {
+
+			if (i < len - n) {
+				newOne[i] = array[i + n];
+
+			} else {
+				newOne[i] = array[c];
+				c = c + 1;
+			}
+		}return newOne;
 	}
+
 
 	/*
 	  5. Balanced Brackets
@@ -62,9 +131,24 @@ public class Lab {
 	  Return false if not balanced
 	*/
 	public static boolean balancedBrackets(String bracketsString) {
-		return false;
+
+		int sL = bracketsString.length();
+		int sum = 0;
+		Boolean torf = false;
+		if (sL < 2 || sL % 2 != 0) {
+			return false;
+		}
+		else {
+		//	int half = sL / 2;
+		//	for (int i = 0; i<= half; i++) {
+				char x = bracketsString.charAt(0);
+				char y = bracketsString.charAt(sL-1);
+
+				if ((x == '(' && y == ')') || (x == '[' && y == ']') || (x == '{' && y == '}')) {
+					torf = true;
+				}
+			}
+		//}
+		return torf;
 	}
-
-
-
 }
