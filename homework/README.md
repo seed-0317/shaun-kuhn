e25fe37681a5d52e4f14d7129a141753a1b385eb
Kuhn <vso513@784f437f9a8c.cof.ds.capitalone.com>
2017-04-18 11:11:03 -0400

    adding homework directory

---
+++ homework/README.md
@@ -0,0 +1,32 @@
+# Homework
+
+Copy this `homework` directory to your personal repo.  Write code such that all tests pass.
+
+### 1. Install node and npm
+
+```
+brew install node
+```
+
+```
+node -v
+npm -v
+```
+
+### 2. `cd` to this directory and run install
+
+```
+npm install
+```
+
+### 3. Install Jasmine globally
+
+```
+npm install -g jasmine
+```
+
+### 4. Run all tests
+
+```
+jasmine
+```
---
+++ tiger/src/main/java/com/example/servlets/DogServlet.java
@@ -0,0 +1,21 @@
+package com.example.servlets;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by vso513 on 4/13/17.
+ */
+@WebServlet(value = "/cow")
+public class DogServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
